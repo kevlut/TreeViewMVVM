ef44ed1fc18d98b5298a7eda2f6fa01b8ae0f929
kevlut <kevinluty@gmail.com>
2014-07-21 14:02:41 -0500

    Additional Parameters to Device

--- TreeViewMVVM/Models/Device.cs
+++ TreeViewMVVM/Models/Device.cs
@@ -8,5 +8,17 @@ namespace TreeViewMVVM.Models
 {
     public class Device : Base.WorkspaceItem, Interfaces.IDevice
     {
+        public Device()
+        {
+            Parameters = new List<Parameter>() { 
+                new Parameter("A", 0, "A Stuff"),
+                new Parameter("B", 1, "B Stuff"),
+                new Parameter("C", 2, "C Stuff"),
+                new Parameter("D", 3, "D Stuff"),
+                new Parameter("E", 4, "E Stuff"),
+                new Parameter("F", 5, "F Stuff")};
+        }
+
+        public List<Parameter> Parameters { get; set; }
     }
 }
---
+++ TreeViewMVVM/Models/Parameter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewMVVM.Models
+{
+    public class Parameter
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public string AdditionalInfo { get; set; }
+
+        public Parameter(string name, int value, string additionalInfo)
+        {
+            Name = name;
+            Value = value;
+            AdditionalInfo = additionalInfo;
+        }
+
+        public override string ToString()
+        {
+            return "Name= " + Name + ", Value= " + Value + ", AdditionalInfo= " + AdditionalInfo;
+        }
+    }
+}
